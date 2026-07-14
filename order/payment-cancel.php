e4fb3389a4fe8437dcd570ded693fec1798d16aa
zs <soozs-wp24@student.tarc.edu.my>
2026-07-14 20:45:30 +0800

    Delete order directory

--- order/payment-cancel.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../_base.php';
-
-// ----------------------------------------------------------------------------
-
-auth('Member');
-
-temp('info', 'Payment was cancelled. Your cart is still saved.');
-redirect('cart.php');
