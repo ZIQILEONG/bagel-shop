c73bbdcb0367aa58c813a304547b8082a976fc37
ZIQILEONG <ziqileong005@gmail.com>
2026-07-16 07:52:57 +0800

    sync and pull

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '632989354b3f19bcb532a6c1f616b6db908678ac',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '632989354b3f19bcb532a6c1f616b6db908678ac',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v20.3.1',
+            'version' => '20.3.1.0',
+            'reference' => 'bf2c6caf886a88e35f831a68f3b6a49ac85cb357',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
