f5477e4931b9f1316c6c3e2ae22b78542417b485
LAI QIAN HUI <laiqh-wm25@student.tarc.edu.my>
2026-07-22 21:17:27 +0800

    profile update

--- user/profile.php
+++ user/profile.php
@@ -1,17 +1,129 @@
 <?php
-include '../_base.php';
 
-// ----------------------------------------------------------------------------
-
+require '_base.php';
 
 
-// ----------------------------------------------------------------------------
+if(!isset($_SESSION['user'])){
 
-$_title = 'User | Profile';
-include '../_head.php';
+    header("location:login.php");
+    exit;
+
+}
+
+
+$user = $_SESSION['user'];
+
+
+
+if(is_post()){
+
+
+    $new_password = post('password');
+
+
+    if($new_password!=""){
+
+
+        $hash=password_hash(
+            $new_password,
+            PASSWORD_DEFAULT
+        );
+
+
+        $stm=$_db->prepare("
+            UPDATE users
+            SET password=?
+            WHERE id=?
+        ");
+
+
+        $stm->execute([
+            $hash,
+            $user->id
+        ]);
+
+
+
+        echo "Password Updated";
+
+
+    }
+
+
+}
+
+
 ?>
 
-<p>...</p>
 
-<?php
-include '../_foot.php';
+<h2>
+Profile
+</h2>
+
+
+<img 
+src="images/<?= $user->photo ?>"
+width="100">
+
+
+<br>
+
+
+Name:
+
+<?= $user->name ?>
+
+
+<br>
+
+
+Email:
+
+<?= $user->email ?>
+
+
+<br>
+
+
+Phone:
+
+<?= $user->phone_no ?>
+
+
+<br>
+
+
+Role:
+
+<?= $user->role ?>
+
+
+
+<hr>
+
+
+<h3>
+Change Password
+</h3>
+
+
+
+<form method="post">
+
+
+New Password:
+
+<input 
+type="password"
+name="password">
+
+
+<br>
+
+
+<button>
+Update Password
+</button>
+
+
+</form>
